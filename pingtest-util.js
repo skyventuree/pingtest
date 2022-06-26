b72977201f78e1c48517eaf84fa35f77a5e6911c
sora <34794115+skyventuree@users.noreply.github.com>
2022-06-26 18:20:54 +0700

    multiple link support

--- pingtest-util.js
+++ pingtest-util.js
@@ -1,20 +1,32 @@
 document.getElementById("add-url").addEventListener("click", function () {
     var url = document.getElementById("url-text").value;
-    var table = document.getElementById("url-list");
-    if (url.length > 0) {
-        if (!url.match(/^https?:\/\//)) url="http://"+url;
-        var row = table.insertRow(table.rows.length);
-        
-        row.insertCell(0).innerHTML = `<a href="${url}">${url}</a>`;
-        row.insertCell(1);
-        row.insertCell(2);
-        row.insertCell(3).innerHTML = `<button class="btn-remove" onclick="removeRow(this)">X</button>`;
-
-        const urlText = document.getElementById("url-text");
-        urlText.value = "";
-        urlText.focus();
+    if (url.length <= 0) return;
+    if (url.indexOf(",") > -1) {
+        var urls = url.split(",");
+        urls.forEach(function (url) {
+            addUrl(url);
+        });
+    }
+    else {
+        addUrl(url);
     }
 });
+
+function addUrl(url) {
+    var table = document.getElementById("url-list");
+    url = url.toLowerCase();
+    if (!url.match(/^https?:\/\//)) url="http://"+url;
+    var row = table.insertRow(table.rows.length);
+    
+    row.insertCell(0).innerHTML = `<a href="${url}">${url}</a>`;
+    row.insertCell(1);
+    row.insertCell(2);
+    row.insertCell(3).innerHTML = `<button class="btn-remove" onclick="removeRow(this)">X</button>`;
+
+    const urlText = document.getElementById("url-text");
+    urlText.value = "";
+    urlText.focus();
+}
 
 document.getElementById("start-ping").addEventListener("click", function () {
     var table = document.getElementById("url-list");
@@ -41,14 +53,13 @@ function ping(url, row) {
     var timeStart = new Date();
 
     xhr.open("GET", url, true);
-    xhr.timeout = 10000;
+    xhr.timeout = 5000;
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
             var timeEnd = new Date();
             var time = timeEnd.getTime() - timeStart.getTime();
             row.cells[1].innerHTML = xhr.status;
             row.cells[2].innerHTML = time + "ms";
-
             row.cells[3].firstChild.disabled = false;
         }
     }
